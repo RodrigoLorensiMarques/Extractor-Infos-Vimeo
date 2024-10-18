18e6ec92cfeb1c5bfcfd45b4c97feb4674572d63
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2024-10-18 18:49:40 -0300

    Create Extração-Infos-Vimeo.py

---
+++ Extração-Infos-Vimeo.py
@@ -0,0 +1,39 @@
+
+from dic import dicionario_vimeo
+import pprint
+
+
+Videos=[]
+
+Data= dicionario_vimeo['data']
+PrimeiroItem=Data[0]
+
+
+for item in Data:
+    Downloads= item['download']
+
+    Quali_540= ''
+    Quali_720=''
+    Quali_1080=''
+
+    for DicionarioDownload in Downloads:
+        if DicionarioDownload['rendition'] =='540p':
+            Quali_540= DicionarioDownload['link']
+        
+        if DicionarioDownload['rendition'] =='720p':
+            Quali_720= DicionarioDownload['link']
+
+        if DicionarioDownload['rendition'] =='1080p':
+            Quali_1080= DicionarioDownload['link']   
+
+        URI=item['uri']
+        Nome=item['name']
+        Duracao=item['duration']
+   
+
+    dict_Videos= {'uri': URI, 'nome': Nome, 'duracao': Duracao, 'link540p': Quali_540, 'link720p': Quali_720, 'link1080p': Quali_1080}
+
+    Videos.append(dict_Videos)
+
+
+pprint.pprint (Videos)
